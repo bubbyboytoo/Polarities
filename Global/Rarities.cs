19c1b9692ae1d6f06ae5a751fe73e462f987caff
goose <40964328+TDLPoof@users.noreply.github.com>
2025-10-02 19:14:15 -0700

    add ice queen flawless rarity

--- Global/Rarities.cs
+++ Global/Rarities.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Polarities.Core;
 using Terraria;
 using Terraria.ModLoader;
@@ -89,6 +89,7 @@ namespace Polarities.Global
     public class DukeFishronFlawlessRarity : ColorLerpCycleRarity { public override Color[] ColorCycle => new Color[] { new Color(16, 243, 140), new Color(16, 243, 140), new Color(14, 87, 215) }; }
     public class LunaticCultistFlawlessRarity : ColorLerpCycleRarity { public override Color[] ColorCycle => new Color[] { new Color(59, 96, 184), new Color(59, 96, 184), new Color(240, 240, 240), new Color(219, 189, 53) }; }
     public class MartianSaucerFlawlessRarity : ColorLerpCycleRarity { public override Color[] ColorCycle => new Color[] { new Color(210, 210, 210), new Color(110, 254, 125), new Color(97, 200, 225) }; }
+    public class IceQueenFlawlessRarity : ColorLerpCycleRarity { public override Color[] ColorCycle => new Color[] { new Color(210, 210, 210), new Color(210, 220, 255) }; }
 }
 
 
